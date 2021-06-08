7b214a9e60ab5516b64f766231ef442b060c5678
Elizapp <41589068+Elizapp@users.noreply.github.com>
2021-06-07 19:19:16 -0500

    fix

--- src/main/java/miu/edu/attendance/service/BarcodeRecordService.java
+++ src/main/java/miu/edu/attendance/service/BarcodeRecordService.java
@@ -1,11 +1,13 @@
 package miu.edu.attendance.service;
 
 import miu.edu.attendance.domain.BarcodeRecord;
+import miu.edu.attendance.dto.BarcodeRecordDTO;
 
 import java.util.List;
 
 public interface BarcodeRecordService {
     public List<BarcodeRecord> getBarcodeRecordByStudentIdAndSessionId(Integer studentId, Integer sessionId);
     public List<BarcodeRecord> getAll();
+    BarcodeRecord createBarcodeRecord(BarcodeRecordDTO barcodeRecordDTO);
 
 }
